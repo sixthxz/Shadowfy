40f7427301d03d1197c6e2602d51333fd2148054
sixthxz <145708241+sixthxz@users.noreply.github.com>
2025-08-03 23:12:32 -0600

    upupup

---
+++ server.js
@@ -0,0 +1,71 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log('----> Incoming:', req.method, req.originalUrl);
+  next();
+});
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/auth/token', async (req, res) => {
+  const { code, code_verifier, redirect_uri } = req.body;
+
+  const params = new URLSearchParams({
+    grant_type: 'authorization_code',
+    code,
+    redirect_uri,
+    client_id: process.env.CLIENT_ID,
+    client_secret: process.env.CLIENT_SECRET,
+    code_verifier
+  });
+
+  try {
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: params
+    });
+
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Token exchange failed' });
+  }
+});
+
+app.get('/env', (req, res) => {
+  res.json({
+    CLIENT_ID: process.env.CLIENT_ID,
+    REDIRECT_URI: process.env.REDIRECT_URI
+  });
+});
+
+
+app.get('/player', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'player.html'));
+});
+
+
+app.get('*', (req, res, next) => {
+  if (req.originalUrl.includes('.') || req.originalUrl.startsWith('/auth/')) {
+    return next(); // Deja pasar archivos .js, .css, etc.
+  }
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(8000, () => console.log('Auth server running on http://localhost:8000'));
